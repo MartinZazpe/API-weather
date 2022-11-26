2eebfe0b3555f318dc26fc47f8d60d3094430a2f
MartinZazpe <martinzazpe1@gmail.com>
2022-11-26 16:17:02 +0100

    fixing requests to apis

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -8,17 +8,16 @@ module.exports = {
     index: (req, res) => {
 
         var apiKeyVisual = "RNMTQR4HN992YPFDY8EV6QGXM"
-
         axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK/2022-08-11/2022-08-15?key=${apiKeyVisual} `).then((response) => {
-
-            // console.log(response.data.days[0].tempmax)
 
             console.log(response.data)
 
             res.render('index', { response })
 
+
         }).catch((error => {
             console.log(error)
+            res.render('index', { error })
         }))
 
     },
@@ -31,6 +30,7 @@ module.exports = {
 
         let userSearch = req.body.userSearch
 
+        //get coordinates from user
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${userSearch}&appid=${apiKey}`).then((response) => {
             lat = response.data.coord["lat"]
             lon = response.data.coord["lon"]
@@ -38,18 +38,30 @@ module.exports = {
             var date = todayDate.getFullYear() + '-' + (todayDate.getMonth() + 1) + '-' + todayDate.getDate()
             var endDate = todayDate.getFullYear() + '-' + (todayDate.getMonth() + 1) + '-' + (todayDate.getDate() + 4)
 
+            //return search of coordinates
             axios.get(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${lat},${lon}/${date}/${endDate}?key=${apiKeyVisual}`).then((data) => {
 
 
-                // console.log(data.data)
-
                 res.render('index', { foundCountry: data })
+
+                // if (data) {
+                //     res.render('index', { foundCountry: data })
+                // } else if (data == 'You have exceeded the maximum number of daily result records for your account. Please add a credit card to continue retrieving results.') {
+                //     res.render('index')
+                // }
             })
         }).catch((error) => {
-            notFound = {
-                msg: "Sorry, no results. Please try again"
+
+            if (error.response.data.message == 'city not found') {
+                notFound = {
+                    msg: "Sorry, no results. Please try again"
+                }
+                res.render('index', { notFound })
+            } else {
+                console.log(error)
+                res.render('index', { error })
             }
-            res.render('index', { notFound })
+
         })
 
     }
